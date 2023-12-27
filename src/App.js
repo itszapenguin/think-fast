da51f9a4d8cae915eaf8690a354635c4e2971905
Ethan Hsu <itszapenguin@gmail.com>
2023-12-28 01:15:08 +0800

    Added transition and shop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [introIsVisible, setIntroIsVisible] = useState(true);
+
+  const [shopIsVisible, setShopIsVisible] = useState(false);
+
+  const handlePlayClick = () => {
+    setIntroIsVisible(false);
+
+    //timer for shop visibility
+    setTimeout(() => {
+      setShopIsVisible(true);
+    }, 1000);
+  };
+
   return (
-    <div className="Arena"> 
-      <div className="Title-Banner">
-        <p className="Title-Text">THINK FAST</p>
-      </div>
+    <div className="arena"> 
+      {introIsVisible && (
+        <div className={`title-banner ${introIsVisible ? '' : 'hide'}`}>          
+        <p className="title-text">THINK FAST</p>
+        </div>
+      )}
+      {introIsVisible && (
+        <button 
+          className={`play-button ${introIsVisible ? '' : 'hide'}`} 
+          onClick={handlePlayClick}
+        >
+          Play Now
+        </button>
+      )}
+
+      {shopIsVisible && (
+        <div className="shop">
+          Shop
+        </div>
+      )}
+
+
+
+
+
 
     </div>
   );
