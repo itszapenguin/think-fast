5e516375e623876decdadc80697a50549603106f
Ethan Hsu <itszapenguin@gmail.com>
2024-01-01 13:06:00 +0800

    revised champion card

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './ChampionCard.css'
 
 const ChampionCard = ({ championName, championImage, cost, traits}) => {
-  return (
-    <button className="card">
-        <div className="champion-image" background-image = {"../../public/assets/tft-champion/TFT10_Amumu.TFT_Set10.png"}></div>
-        <div className="border">{championName}</div>
-    </button>
-  );
+
+
+    return (
+        <button className="card">
+            <div className="champion-image" background-image = {"../../public/assets/tft-champion/TFT10_Amumu.TFT_Set10.png"}></div>
+            <div className="border">{championName}</div>
+        </button>
+    );
 };
 
 export default ChampionCard;
